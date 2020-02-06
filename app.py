9812dc8f2e19fd5f457c2b5c751754e6073c7eca
KL <d23584d321@gmail.com>
2020-02-06 15:56:44 +0800

    Add code

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "文字":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     
 
 import os
