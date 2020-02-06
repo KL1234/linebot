acd05a1cca9baeb9b974ac0dd0cee6a7af0970be
KL <d23584d321@gmail.com>
2020-02-06 16:15:26 +0800

    Add code

--- app.py
+++ app.py
@@ -34,10 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    if event.message.text == "吃什麼":
+    if event.message.text.find("吃什麼") != -1:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text="吃我的毛"))
-    if event.message.text.find("韓國瑜") != -1:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text="韓國瑜凍蒜"))
     
 
 import os
