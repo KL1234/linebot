73472b5af8435205c34dfe8df309dfc1efa94dd5
KL <d23584d321@gmail.com>
2020-02-11 16:11:41 +0800

    Add code

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
+    import parse_weather
     if event.message.text.find("吃什麼") != -1:
         import random
         #四個一排
