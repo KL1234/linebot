06f9b34fbc0a1cd3a74d2d484fd5522d663c3d8f
KL <d23584d321@gmail.com>
2020-02-24 16:10:42 +0800

    Add code

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    message = message.upper() 
     if event.message.text.find("吃什麼") != -1:
         import random
         #四個一排
